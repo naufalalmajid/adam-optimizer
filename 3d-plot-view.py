447d0721f9f75bbf18e97fd1c0b5406703756099
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2025-05-08 01:51:45 +0700

    Update 3d-plot-view.py
    make possible to import and running obj and der function to adam file

--- 3d-plot-view.py
+++ 3d-plot-view.py
@@ -1,4 +1,3 @@
-#only for plot shape
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,20 +7,26 @@ def obj(x, y):
     return x**2 + 2 * y**2 + 5 * x + 12 * y + 22
 
 
-# grid values
-xAxis = np.arange(-12.5, 7.5, 0.1)
-yAxis = np.arange(-13, 7, 0.1)
-x, y = np.meshgrid(xAxis, yAxis)
-z = obj(x, y)
+# der func
+def der(x, y):
+    return np.array([2 * x + 5, 4 * y + 12])
 
-# create a 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
-surface = ax.plot_surface(x, y, z, cmap="ocean")
 
-ax.set_title("3D Surface Plot of the Objective Function")
-ax.set_xlabel("X-axis")
-ax.set_ylabel("Y-axis")
-ax.set_zlabel("Z-axis (Function Value)")
+if __name__ == "__main__":
+    # grid values
+    xAxis = np.arange(-12.5, 7.5, 0.1)
+    yAxis = np.arange(-13, 7, 0.1)
+    x, y = np.meshgrid(xAxis, yAxis)
+    z = obj(x, y)
 
-plt.show()
+    # create a 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    surface = ax.plot_surface(x, y, z, cmap="ocean", alpha=0.5)
+
+    ax.set_title("3D Surface Plot of the Objective Function")
+    ax.set_xlabel("X-axis")
+    ax.set_ylabel("Y-axis")
+    ax.set_zlabel("Z-axis (Function Value)")
+
+    plt.show()
